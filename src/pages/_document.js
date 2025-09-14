f8502a8147816702db4a674c90122e60154b27f1
Joshtri <stuffofyos1516@gmail.com>
2025-09-14 08:11:22 +0700

    Commit: _document.js

---
+++ src/pages/_document.js
@@ -0,0 +1,14 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head />
+      <title>GMIT Imanuel Oepura</title>
+      <body className="antialiased">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}

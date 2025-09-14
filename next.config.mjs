9b6e046debb89ae7353861e53c1d1c9ee9e1c21c
Joshtri <stuffofyos1516@gmail.com>
2025-09-14 08:11:08 +0700

    Commit: next.config.mjs

---
+++ next.config.mjs
@@ -0,0 +1,37 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  
+  // Fix untuk messaging timeout issues
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '5mb',
+    },
+  },
+  
+  // WebSocket configuration untuk development
+  devIndicators: {
+    position: 'bottom-right',
+  },
+  
+  // Optimisasi untuk development
+  onDemandEntries: {
+    // period (in ms) where the server will keep pages in the buffer
+    maxInactiveAge: 25 * 1000,
+    // number of pages that should be kept simultaneously without being disposed
+    pagesBufferLength: 2,
+  },
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 's3.nevaobjects.id',
+        port: '',
+        pathname: '/files-bucket/**',
+      },
+    ],
+  },
+};
+
+export default nextConfig;

b15da88ee3618bafb5ed3471daaa2881cf506a4b
Joshtri <stuffofyos1516@gmail.com>
2025-09-14 08:11:12 +0700

    Commit: history.js

---
+++ src/components/about/history.js
@@ -0,0 +1,16 @@
+export default function History() {
+  return (
+    <div className="flex flex-col justify-center items-center p-8 gap-4 min-h-[50vh] bg-gray-900">
+      <h2 className="text-4xl font-bold">Our History</h2>
+      <p className="text-lg leading-relaxed text-justify">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi iaculis est erat, eget lacinia elit varius in. Ut convallis dolor massa, sit amet consectetur justo consequat vitae. Nullam scelerisque erat viverra elit aliquam, a rhoncus justo accumsan. Morbi et tellus ac neque malesuada venenatis ac nec augue. Quisque nibh enim, cursus ut purus ac, facilisis luctus velit. Vivamus sit amet dignissim purus, sit amet imperdiet metus. Nam rutrum dui ante. In aliquam vestibulum nibh, quis sollicitudin eros suscipit et. Curabitur vestibulum ut odio in malesuada. Sed vehicula congue enim vitae viverra.
+      </p>
+      <p className="text-lg leading-relaxed text-justify">
+        Fusce in eleifend arcu. Sed placerat tincidunt tellus, quis pretium neque vulputate vitae. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse dapibus tempor lacus, vitae laoreet magna semper ut. Aenean leo nulla, venenatis et ligula gravida, finibus rhoncus purus. Maecenas blandit pulvinar tristique. Maecenas ullamcorper tempus pharetra. Cras tincidunt magna vel turpis feugiat rhoncus. Nulla id sem quis quam placerat rhoncus a vel nibh. Fusce commodo lobortis felis in pretium. Curabitur consectetur laoreet lacus in aliquam. Suspendisse vitae lectus et nisl aliquam tristique vitae sit amet urna.
+      </p>
+      <p className="text-lg leading-relaxed text-justify">
+        Ut pulvinar lectus in vulputate condimentum. Integer finibus a ipsum quis laoreet. Fusce quis hendrerit nisl, non lacinia libero. Morbi augue ipsum, ultrices ut est vel, aliquet maximus nulla. Integer suscipit tristique faucibus. Fusce viverra mi in dignissim tincidunt. Phasellus ac neque vel lacus tristique condimentum. Aenean in vehicula risus. Maecenas finibus orci vel imperdiet feugiat. Aenean dui neque, ullamcorper ut sem quis, semper malesuada ligula. Donec vitae nibh neque. Phasellus malesuada magna at maximus sagittis. Pellentesque iaculis eros eu enim varius, et dapibus magna interdum. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      </p>
+    </div>
+  );
+}
